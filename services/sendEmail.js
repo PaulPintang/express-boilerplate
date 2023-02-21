1ab5b7d11402bc78132509b453361671b84ce19a
PaulPintang <pauljpintang@gmail.com>
2023-02-21 13:23:08 +0800

    fix: email send twice

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -3,7 +3,7 @@ dotenv.config();
 const nodemailer = require("nodemailer");
 
 // async..await is not allowed in global scope, must use a wrapper
-const sendEmail = async (data) => {
+const sendEmail = (data) => {
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -13,19 +13,11 @@ const sendEmail = async (data) => {
   });
 
   // send mail with defined transport object
-  let info = await transporter.sendMail({
+  transporter.sendMail({
     from: process.env.EMAIL, // sender address
     to: data.email, // list of receivers
     subject: "Email verification", // Subject line
     html: `${data.OTP} is your one time passcode(OTP) to reset your account password`, // html body
-  });
-
-  transporter.sendMail(info, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("email sent");
-    }
   });
 };
 
